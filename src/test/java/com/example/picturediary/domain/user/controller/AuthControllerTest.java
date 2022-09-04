5a174b2d5d78a558125661a5c19dafbe251d7335
문지은 <s2moon98@naver.com>
2022-09-04 16:02:40 +0900

    feature-#45 : 테스트용 설정 추가

--- src/test/java/com/example/picturediary/domain/user/controller/AuthControllerTest.java
+++ src/test/java/com/example/picturediary/domain/user/controller/AuthControllerTest.java
@@ -14,6 +14,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpHeaders;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -27,6 +28,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @Transactional
 @SpringBootTest
 @AutoConfigureMockMvc
+@ActiveProfiles("test")
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class AuthControllerTest
 {
